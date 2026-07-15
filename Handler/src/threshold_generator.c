410f6cf1b2fbde86a661916a617b73413d64f2ca
Damwrtq <danielwolfenson483@gmail.com>
2026-07-15 16:30:52 +0300

    Threshold

---
+++ Handler/src/threshold_generator.c
@@ -0,0 +1,24 @@
+#include "threshold.h"
+
+int threshold_generator (struct GlobalProcessingParam *par, struct data *in, struct Codogramm *thr)
+{
+  /* Если параметр включения = 0, то выход из функции */
+  if (par->threshold.enable == 0) {
+    printf("turned off");
+    return 1;
+  }
+
+  /* Цикл заполнения промежуточной структуры пороговыми значениями */
+  int i;
+  for(i = 0; i < 1024; i++){
+    thr->sign[i].threshold = par->threshold.threshold;
+  }
+  /**********************************
+  thr->azimuth = in->azimuth;
+  thr->worktime = in->worktime;
+  thr->index = in->index;
+  thr->sample_size = in->sample_size;
+  **********************************/
+
+  return 0;
+}
