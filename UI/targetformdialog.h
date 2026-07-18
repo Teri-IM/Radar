20924ad0abe0f2c3ae8294201640067de2bb78b6
KROKUSO <azxdswqed123azxdswqed@gmail.com>
2026-07-18 16:44:18 +0300

    ui3

--- UI/targetformdialog.h
+++ UI/targetformdialog.h
@@ -10,13 +10,17 @@
 
 // Структура формуляра одной цели
 struct TargetInfo {
-    int number = 0;          // номер цели
-    QString name;             // название/метка цели
-    double range = 0.0;       // дальность, м
-    double azimuth = 0.0;     // азимут, град
-    double speed = 0.0;       // скорость, м/с
-    double amplitude = 0.0;   // амплитуда отклика
-    bool active = true;       // активна ли цель
+    int number;        // номер цели
+    QString name;       // название/метка цели
+    double range;       // дальность, м
+    double azimuth;     // азимут, град
+    double speed;       // скорость, м/с
+    double amplitude;   // амплитуда отклика
+    bool active;        // активна ли цель
+
+    TargetInfo(int num = 0, const QString &n = QString(), double r = 0.0,
+               double az = 0.0, double sp = 0.0, double amp = 0.0, bool act = true)
+        : number(num), name(n), range(r), azimuth(az), speed(sp), amplitude(amp), active(act) {}
 };
 
 class TargetFormDialog : public QDialog
