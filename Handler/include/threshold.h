c08eaf12fd785be57b1519c349d7cc935ba50205
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2026-07-21 13:19:42 +0300

    comments

--- Handler/include/threshold.h
+++ Handler/include/threshold.h
@@ -19,25 +19,25 @@ typedef struct input_signal_s {
     int index;
     int sample_size;
     sample_t sample[CHUNK_SIZE];
-} input_signal_t;
+} input_signal_t; */
 
 /* Структура, описывающая пороговый сигнал
  * (будет заполнена пороговыми значениями)
 typedef struct threshold_signal_s {
-    /***************
+    ***************
     int azimuth;
     int worktime;
     int index;
     int sample_size;
     **************
     sample_t sample[CHUNK_SIZE];
-} threshold_signal_t;
+} threshold_signal_t; */
 
 /* Структура, описывающая обработанный сигнал (упрощенная для данного модуля)
 typedef struct output_signal_s {
     int azimuth;
     sample_t sample;
-} output_signal_t;
+} output_signal_t; */
 
 /* Конструкция, позволяющая запускать c-программы на компиляторе c++
  * (те конструкции, которые поддерживаются только на c нельзя просто так
